3596848271d8916b7001bfd955e2071c5d3d4a9d
FalconRay <falconray@yahoo.com>
2017-07-04 18:58:45 +0800

    init version for universal grpc

---
+++ gRPC/cpp/uGrpc/uGrpc_server.cc
@@ -0,0 +1,157 @@
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpc/grpc.h>
+#include <grpc++/server.h>
+#include <grpc++/server_builder.h>
+#include <grpc++/server_context.h>
+#include <grpc++/security/server_credentials.h>
+#include "helper.h"
+#include "uGrpc.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerReader;
+using grpc::ServerReaderWriter;
+using grpc::ServerWriter;
+using grpc::Status;
+using uGrpc::EmbUArgs;
+using uGrpc::EmbUResponse;
+using uGrpc::UArgs;
+using uGrpc::UResponse;
+using std::chrono::system_clock;
+
+std::string GetFeatureName(const Point& point, const std::vector<Feature>& feature_list) 
+{
+  for (const Feature& f : feature_list) {
+    if (f.location().latitude() == point.latitude() &&
+        f.location().longitude() == point.longitude()) {
+      return f.name();
+    }
+  }
+  return "";
+}
+
+class UGrpcImpl final : public uGrpc::Service 
+{
+ public:
+  explicit UGrpcImpl(const std::string& db) 
+  {
+    routeguide::ParseDb(db, &feature_list_);
+  }
+
+  Status GetFeature(ServerContext* context, const Point* point, Feature* feature) override 
+  {
+    feature->set_name(GetFeatureName(*point, feature_list_));
+    feature->mutable_location()->CopyFrom(*point);
+    return Status::OK;
+  }
+
+  Status ListFeatures(ServerContext* context, const uGrpc::Rectangle* rectangle, ServerWriter<Feature>* writer) override 
+  {
+    auto lo = rectangle->lo();
+    auto hi = rectangle->hi();
+    long left = (std::min)(lo.longitude(), hi.longitude());
+    long right = (std::max)(lo.longitude(), hi.longitude());
+    long top = (std::max)(lo.latitude(), hi.latitude());
+    long bottom = (std::min)(lo.latitude(), hi.latitude());
+    for (const Feature& f : feature_list_) {
+      if (f.location().longitude() >= left &&
+          f.location().longitude() <= right &&
+          f.location().latitude() >= bottom &&
+          f.location().latitude() <= top) {
+        writer->Write(f);
+      }
+    }
+    return Status::OK;
+  }
+
+  Status RecordRoute(ServerContext* context, ServerReader<Point>* reader, RouteSummary* summary) override 
+  {
+    Point point;
+    int point_count = 0;
+    int feature_count = 0;
+    float distance = 0.0;
+    Point previous;
+
+    system_clock::time_point start_time = system_clock::now();
+    while (reader->Read(&point)) {
+      point_count++;
+      if (!GetFeatureName(point, feature_list_).empty()) {
+        feature_count++;
+      }
+      if (point_count != 1) {
+        distance += GetDistance(previous, point);
+      }
+      previous = point;
+    }
+    system_clock::time_point end_time = system_clock::now();
+    summary->set_point_count(point_count);
+    summary->set_feature_count(feature_count);
+    summary->set_distance(static_cast<long>(distance));
+    auto secs = std::chrono::duration_cast<std::chrono::seconds>(
+        end_time - start_time);
+    summary->set_elapsed_time(secs.count());
+
+    return Status::OK;
+  }
+
+  Status RouteChat(ServerContext* context, ServerReaderWriter<RouteNote, RouteNote>* stream) override 
+  {
+    std::vector<RouteNote> received_notes;
+    RouteNote note;
+    while (stream->Read(&note)) {
+      for (const RouteNote& n : received_notes) {
+        if (n.location().latitude() == note.location().latitude() &&
+            n.location().longitude() == note.location().longitude()) {
+          stream->Write(n);
+        }
+      }
+      received_notes.push_back(note);
+    }
+
+    return Status::OK;
+  }
+
+ private:
+
+  std::vector<Feature> feature_list_;
+};
+
+void RunServer(const std::string& db_path) 
+{
+  std::string server_address("0.0.0.0:50051");
+  UGrpcImpl service(db_path);
+
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+  server->Wait();
+}
+
+int main(int argc, char** argv) 
+{
+  // Expect only arg: --db_path=path/to/route_guide_db.json.
+  std::string db = uGrpc::GetDbFileContent(argc, argv);
+  RunServer(db);
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
